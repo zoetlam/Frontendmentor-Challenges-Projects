e1ac2856848f592a3cfbc2e7a4130dcd6fe946a0
Thi Lam <z.thiilam@gmail.com>
2024-11-08 23:09:17 -0600

    install tailwind css

---
+++ base-apparel-coming-soon-master/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",               // Path to your HTML file(s)
+    "./assets/css/**/*.{css,html}" // Path to your CSS and any other files where Tailwind classes might appear
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
