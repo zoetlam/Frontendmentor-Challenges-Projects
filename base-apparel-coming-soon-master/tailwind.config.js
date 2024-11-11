4f9a13e540ef77e8510b0e9beac00fa24211178b
Thi Lam <z.thiilam@gmail.com>
2024-11-11 17:49:59 -0600

    update folder

--- base-apparel-coming-soon-master/tailwind.config.js
+++ base-apparel-coming-soon-master/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{html,js}"
+    "./index.html",               // Path to your HTML file(s)
+    "./assets/css/**/*.{css,html}" // Path to your CSS and any other files where Tailwind classes might appear
   ],
   theme: {
     extend: {},
