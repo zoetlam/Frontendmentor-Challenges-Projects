10a7bb13c86f3092ca73b6738cc4232012ce1272
Thi Lam <z.thiilam@gmail.com>
2025-06-30 17:42:35 -0500

    🎉 init: kick off mortgage calculator with Express & EJS

---
+++ mortgage-repayment-calculator-main/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
